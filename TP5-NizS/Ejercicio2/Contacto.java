9720ce43c2cd3995f8abdb944ba781018583def3
sgn912 <santiagoniz4@gmail.com>
2026-06-25 17:48:41 -0300

    Update Contacto.java

---
+++ TP5-NizS/Ejercicio2/Contacto.java
@@ -0,0 +1,25 @@
+package resol.NizS.ejercicio2;
+
+public class Contacto {
+
+    private String nombre;
+    private String telefono;
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - " + telefono;
+    }
+}
